9660582b8a35cbab6f0e4a56c3beaeb8d2c0df0c
inglkruiz <ing.lkruiz@gmail.com>
2017-05-14 16:41:55 -0600

    Go to page # on Paginator

--- src/App.js
+++ src/App.js
@@ -1,17 +1,33 @@
 import React, { Component } from 'react';
 import './App.css';
+import Header from './components/header';
 import Character from './components/character';
 import Paginator from './components/paginator';
-import Header from './components/header';
 
 const getMarvelCharactersCall = (offset) =>
   fetch(`https://gateway.marvel.com/v1/public/characters?apikey=${process.env.REACT_APP_PUBLIC_API_KEY}&offset=${offset}`)
     .then(res => res.json())
     .then((resObj) => {
-      if (resObj.code === 200) {
-        return resObj.data.results;
-      } else {
-        return [];
+      try {
+        if (resObj.code === 200) {
+          if (offset > resObj.data.total) {
+            throw new Error('Page does not exist.');
+          } else {
+            const pages = Math.floor(resObj.data.total / 20);
+            return {
+              characters: resObj.data.results,
+              maxPage: resObj.data.total % 20 > 0 ? pages+1 : pages,
+            };
+          }
+        } else {
+          throw new Error(`Marvel API bad response. Status code ${resObj.code}.`);
+        }
+      } catch(e) {
+        console.error(e);
+        return {
+          characters: [],
+          maxPage: 0,
+        };
       }
     });
 
@@ -19,19 +35,20 @@ class App extends Component {
   state = {
     characters: [],
     page: 0,
+    maxPage: 0,
   };
 
   componentWillMount() {
-    getMarvelCharactersCall(0).then((characters) => {
-      this.setState({ characters, page: 1 });
+    getMarvelCharactersCall(0).then(({characters, maxPage}) => {
+      this.setState({ characters, page: 1, maxPage });
     });
   }
 
   changePage = (page) => {
     if(page !== this.state.page) {
       const offset = (page-1)*20;
-      getMarvelCharactersCall(offset).then((characters) => {
-        this.setState({ characters, page });
+      getMarvelCharactersCall(offset).then(({ characters, maxPage }) => {
+        this.setState({ characters, page, maxPage });
       });
     }
   }
@@ -47,6 +64,7 @@ class App extends Component {
   }
 
   render() {
+    //TODO: Define an error messages container.
     return (
       <div className="App">
         <Header />
@@ -62,7 +80,8 @@ class App extends Component {
               // .filter(c => /^(.(?!image_not_available$))+$/.test(c.thumbnail.path))
               .map(c => <Character key={c.id} instance={c} />)
           }
-          <Paginator current={this.state.page}
+          <Paginator page={this.state.page}
+                     maxPage={this.state.maxPage}
                      onChangePage={this.changePage}
                      onNext={this.nextPages}
                      onPrevious={this.previousPages} />
--- src/components/paginator/index.js
+++ src/components/paginator/index.js
@@ -1,70 +1,125 @@
-import React, { Component } from 'react';
+import React, {Component} from 'react';
 import PropTypes from 'prop-types';
 import PageButton from './pageButton';
 import './style.css';
 
-function getPages(current) {
-  const pages = [];
-  if(current > 0) {
-    for(let i = 0; i < 5 ; i++) {
-      pages.push(current+i);
-    }
+const onlyNumbers = (event) => {
+  const key = event.charCode || event.keyCode || 0;
+  if (!(
+      key === 8 ||
+      key === 9 ||
+      key === 13 ||
+      key === 46 ||
+      (key >= 35 && key <= 40) ||
+      (key >= 48 && key <= 57) ||
+      (key >= 96 && key <= 105)
+    )) {
+    event.preventDefault();
   }
-  return pages;
-}
+};
 
 class Paginator extends Component {
-  state = {
-    pages: getPages(this.props.current),
-  };
+  constructor(props) {
+    super(props);
+    this.state = {
+      pages: [],
+    };
+  }
+
+  componentWillMount() {
+    this.setState({ pages: this.setPages(this.props.page, this.props.maxPage)})
+  }
+
+  setPages = (page, maxPage) => {
+    const pages = [];
+    if (page > 0) {
+      for (let i = 0; i < 5; i++) {
+        const p = page + i;
+        if (p <= maxPage) {
+          pages.push(p);
+        }
+      }
+    }
+    return pages;
+  }
 
   componentWillReceiveProps(nextProps) {
-    if(this.state.pages.indexOf(nextProps.current) === -1) {
-      this.setState({ pages: getPages(nextProps.current)});
+    if (this.state.pages.indexOf(nextProps.page) === -1) {
+      this.setState({pages: this.setPages(nextProps.page, nextProps.maxPage)});
     }
   }
 
   previous = () => {
     const minPage = this.state.pages[0];
-    if(minPage > 1) {
+    if (minPage > 1) {
       this.setState({
-        pages: getPages(minPage-5),
+        pages: this.setPages(minPage - 5),
       });
       this.props.onPrevious(minPage);
     }
   }
 
+  handleChange = (event) => {
+    // event.preventDefault();
+    if(event.key === 'Enter') {
+      const page = +event.target.value;
+      try {
+        if (isNaN(page) || page === 0 || page > this.props.maxPage) { throw new Error('Invalid number page.'); }
+        else { this.props.onChangePage(page); }
+      } catch (e) {
+        console.error(e);
+        this.refs.input.value = '';
+      }
+    }
+  }
+
   render() {
     return (
-      <nav className="text-center Paginator" aria-label="Page navigation">
-        <ul className="pagination pagination-lg">
-          <li>
-            <a role="button" aria-label="Previous"
-               onClick={this.previous}>
-              <span aria-hidden="true">&laquo;</span>
-            </a>
-          </li>
-          {
-            this.state.pages.map((i) =>
-              <PageButton key={`page-${i}`}
-                          page={i}
-                          current={this.props.current}
-                          clickHandler={this.props.onChangePage}/>)
-          }
-          <li>
-            <a role="button" aria-label="Next"
-               onClick={() => this.props.onNext(this.state.pages[4])}>
-              <span aria-hidden="true">&raquo;</span>
-            </a>
-          </li>
-        </ul>
-      </nav>
+      <div className="Paginator">
+        <div className="form-group-lg text-center center-block Paginator__go2Page">
+          <label htmlFor="go2Page">Go to Page</label>
+          <input id="go2Page"
+                 ref="input"
+                 type="text"
+                 maxLength={2}
+                 className="form-control text-center"
+                 onKeyDown={onlyNumbers}
+                 onKeyUp={this.handleChange}/>
+          <small className="help-block">
+            Page {this.props.page} out of {this.props.maxPage}.
+          </small>
+        </div>
+        <nav className="text-center" aria-label="Page navigation">
+          <ul className="pagination pagination-lg">
+            <li>
+              <a role="button" aria-label="Previous"
+                 onClick={this.previous}>
+                <span aria-hidden="true">&laquo;</span>
+              </a>
+            </li>
+            {
+              this.state.pages.map((i) =>
+                <PageButton key={`page-${i}`}
+                            page={i}
+                            current={this.props.page}
+                            clickHandler={this.props.onChangePage}/>)
+            }
+            <li>
+              <a role="button" aria-label="Next"
+                 onClick={() => this.props.onNext(this.state.pages[4])}>
+                <span aria-hidden="true">&raquo;</span>
+              </a>
+            </li>
+          </ul>
+        </nav>
+      </div>
     );
   }
 }
 
 Paginator.propTypes = {
-  current: PropTypes.number.isRequired,
+  page: PropTypes.number.isRequired,
+  maxPage: PropTypes.number.isRequired,
   onChangePage: PropTypes.func,
   onNext: PropTypes.func,
   onPrevious: PropTypes.func,
